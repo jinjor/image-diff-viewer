c463102014b583b78b4baaaa37f82ea92670cdb7
Vasil Vanchuk <v.vanchuk@tut.by>
2020-07-03 22:01:47 +0300

    Add pixelmatch

--- src/generator.ts
+++ src/generator.ts
@@ -57,6 +57,7 @@ function generateHtml(fileDiffs: FileDiffs, paths: Paths): string {
   }
   let html = `
 ${styleTag}
+<script src="https://bundle.run/pixelmatch"></script>
 <h1>Changes</h1>
 `;
   let rows = "";
@@ -82,6 +83,69 @@ function generateRow(file: string, fileDiff: FileDiff, paths: Paths): string {
   html += generateColumn(fileDiff, "left", paths);
   html += generateColumn(fileDiff, "right", paths);
   html += `</div>\n`;
+  html += `
+  <script>
+  var canvas = document.createElement('canvas');
+
+  var ctx = canvas.getContext('2d');
+
+  function getImageAsImageData(imageObj) {
+    return new Promise((resolve) => {
+      var canvas = document.createElement('canvas');
+
+      var context = canvas.getContext('2d');
+
+      imageObj.onload = function () {
+        const { width, height } = imageObj;
+        canvas.width = width;
+        canvas.height = height;
+
+        context.drawImage(imageObj, 0, 0, width, height);
+        imgData = context.getImageData(0, 0, width, height);
+        canvas.width = 1;
+        canvas.height = 1;
+
+        resolve(imgData);
+      };
+      const src = imageObj.src;
+      imageObj.src = '';
+      imageObj.src = src;
+    });
+  }
+
+  document.querySelectorAll('.row').forEach((rowEl) => {
+    const images = rowEl.querySelectorAll('img');
+    if (!images.length === 2) {
+      return;
+    }
+
+
+
+
+    Promise.all(
+      [
+        getImageAsImageData(images[0]),
+        getImageAsImageData(images[1]),
+      ]
+    ).then(([img1, img2]) => {
+      const { width, height } = images[1] || images[0];
+      const newCol = document.createElement('div');
+      newCol.classList.add('col');
+      newCol.innerHTML = '<div class="image-container"><canvas /></div>';
+      rowEl.appendChild(newCol);
+
+      const diffCanvas = rowEl.querySelector('canvas');
+      diffCanvas.width = width;
+      diffCanvas.height = height;
+      const diffContext = diffCanvas.getContext('2d');
+
+      const diff = ctx.createImageData(width, height);
+      pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
+      diffContext.putImageData(diff, 0, 0);
+    });
+  });
+</script>
+`
   return html;
 }
 
